2e47e12bf651d735ad85af02ac9fdd58b868c94c
Camilo <camilobu1.9@gmail.com>
2019-08-18 20:44:34 +1000

    Primer Comit

---
+++ server/utilidades/utilidades.js
@@ -0,0 +1,11 @@
+const crearMensaje = (nombre, mensaje) => {
+    return {
+        nombre,
+        mensaje,
+        fecha: new Date().getDate()
+    }
+};
+
+module.exports = {
+    crearMensaje
+}
